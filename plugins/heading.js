1e7a8f4890e01c619aa6bd777b114eefe0054acf
TeemoPeng <danielpog@163.com>
2020-12-03 09:26:14 +0800

    heading插件重构

--- plugins/heading.js
+++ plugins/heading.js
@@ -1,27 +1,37 @@
+// heading插件
 const heading = {
+    // 注册插件，需暴露一个install方法
     install(Vue, options) {
-        // <h3> <svg><use xlink:use='#/icon-xxx'></use></svg> </h3>
+        // heading 最终渲染结果：<h3 title='xxx'><svg><use xlink:use='xxx'></use></svg> xxx </h3>
         Vue.component('heading', {
-            functional: true,
+            functional: true, // 函数式组件（无状态组件）
             render(h, context) {
+                const {title, level, icon} = context.props
                 let children = []
-                const {level, title, icon} = context.props
-                console.log('context:', context)
+
                 if (icon) {
-                    children.push(
-                        h('svg', {}, [
-                            h('use', 
-                                { attrs: {'xlink:use': icon }}
-                            )
-                        ]))
-                    children = children.concat(context.children)
+                    children.push(h('svg', {}, [
+                        h('use', {
+                            attrs: {
+                                'xlink:use': icon
+                            }
+                        })
+                    ]))
+
+                    children = children.concat(context.children) // context.children: h 子元素， slots
                 }
-                return h('h' + level, { attrs: { title: title } }, children)
+
+                return h('h' + level, {
+                    attrs: {
+                        title: title
+                    }
+                }, children)
             }
         })
     }
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
+    // 使用插件
     window.Vue.use(heading)
 }
